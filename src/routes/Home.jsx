6351886d5261606fb1085f359b4f34997b8a355d
pablo <wademcfild@gmail.com>
2023-11-23 10:39:42 -0300

    ...

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,18 +1,16 @@
 import { useState, useEffect } from "react";
-import axios from "axios";
+import blogFetch from "../axios/config";
 
 import { Link } from "react-router-dom";
 
-// import "./Home.css";
+import "./Home.css";
 
 const Home = () => {
   const [posts, setPosts] = useState([]);
 
   const getPosts = async () => {
     try {
-      const response = await axios.get(
-        "https://jsonplaceholder.typicode.com/posts"
-      );
+      const response = await blogFetch.get("/posts");
 
       const data = response.data;
 
@@ -27,8 +25,8 @@ const Home = () => {
   }, []);
 
   return (
-    <div>
-      <h1>Últimos postws</h1>
+    <div className="home">
+      <h1>Últimos postes</h1>
       {posts.length === 0 ? (
         <p>Carregando...</p>
       ) : (
@@ -36,7 +34,7 @@ const Home = () => {
           <div className="post" key={post.id}>
             <h2>{post.title}</h2>
             <p>{post.body}</p>
-            <Link to={`/posts/${post.id}`}>Ler mais</Link>
+            <Link to={`/posts/${post.id}`} className="btn">Ler mais</Link>
           </div>
         ))
       )}
