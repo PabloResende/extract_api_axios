b67d379a93c316443d14634c57ae5b3d5e127e8a
pablo <wademcfild@gmail.com>
2023-11-23 10:55:51 -0300

    ...

--- src/routes/NewPost.jsx
+++ src/routes/NewPost.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+// import React from "react";
+import "./NewPost.css";
 
 const NewPost = () => {
   return (
-    <div>
-      <h1>newpost</h1>
+    <div className="new-post">
+      <h2>Inserir novo post:</h2>
+      <form>
+        <div className="form-control">
+          <label htmlFor="title">Título do post:</label>
+          <input
+            type="text"
+            name="title"
+            id="title"
+            placeholder="Digite um título"
+          />
+        </div>
+        <div className="form-control">
+          <label htmlFor="body">Conteúdo:</label>
+          <textarea
+            name="body"
+            id="body"
+            placeholder="Digite um conteúdo:"
+          ></textarea>
+        </div>
+        <input type="button" value={"criar post"} className="btn" />
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default NewPost
+export default NewPost;
